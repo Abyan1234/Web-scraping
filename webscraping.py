e77ee59c502afcabe9701dc455927fe772ad9f92
Abyan1234 <70338733+Abyan1234@users.noreply.github.com>
2021-11-28 18:36:35 -0500

    Add files via upload

---
+++ webscraping.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import csv
+
+START_URL="https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+browser=webdriver.Chrome("/Users/Owner/Downloads/chromedriver")
+browser.get(START_URL)
+time.sleeep(10)
+
+def scrape():
+    headers=["v_mag._(mv)","proper_name","bayer_designation","distance_(ly)","spectral_class","mass_(m☉)","radius_(r☉)","luminosity_(l☉)"]
+    star_data=[]
+    for i in range(0,458):
+        soup=BeautifulSoup(browser.page_source,"html.parser")
+        for ul_tag in soup.find_all("ul",attrs={"class","stars"}):
+            li_tags=ul_tag.find_all("li")
+            temp_list=[]
+            for index,li_tag in enumerate(li_tags):
+                if index == 0:
+                    temp_list.append(li_tag.find_all("a")[0].contents[0])
+                else:
+                    try:
+                        temp_list.append(li_tag.contents[0])
+                    except:
+                        temp_list.append("")
+            star_data.append(temp_list)
+        browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+    with open("scraper_to.csv","w") as f:
+        csvwriter=csv.writer(f)
+        csvwriter.writerow(headers)
+        csvwriter.writerows(star_data)
+scrape()
